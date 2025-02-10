0470bc3a8d4fe12911f6e815c657e4a3117a7cd6
adambor <adamborteam@gmail.com>
2025-02-10 20:22:40 +0100

    Use v1 instead of v2 for account deployment txs

--- src/starknet/base/modules/StarknetAccounts.ts
+++ src/starknet/base/modules/StarknetAccounts.ts
@@ -6,8 +6,10 @@ import {DeployAccountContractPayload} from "starknet";
 export class StarknetAccounts extends StarknetModule {
 
     public async getAccountDeployTransaction(deploymentData: DeployAccountContractPayload): Promise<StarknetTx> {
+        const feeDetails = this.root.Fees.getFeeDetails(5000, 0, await this.root.Fees.getFeeRate());
         const details = {
-            ...this.root.Fees.getFeeDetails(5000, 0, await this.root.Fees.getFeeRate()),
+            ...feeDetails,
+            version: (feeDetails.version==="0x2" ? "0x1" : feeDetails.version) as "0x1" | "0x2" | "0x3",
             walletAddress: deploymentData.contractAddress,
             cairoVersion: "1" as const,
             chainId: this.root.starknetChainId,

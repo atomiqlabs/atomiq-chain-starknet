bcfc9a4117e906859a2a39e8248d9488ef2343f4
adambor <adamborteam@gmail.com>
2025-03-30 11:57:43 +0200

    Fix provider with timeout

--- dist/starknet/provider/RpcProviderWithRetries.js
+++ dist/starknet/provider/RpcProviderWithRetries.js
@@ -1,21 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.RpcProviderWithRetries = void 0;
+exports.RpcProviderWithRetries = exports.RpcChannelWithRetries = void 0;
 const starknet_1 = require("starknet");
 const Utils_1 = require("../../utils/Utils");
-class RpcProviderWithRetries extends starknet_1.RpcProvider {
+class RpcChannelWithRetries extends starknet_1.RpcChannel {
     constructor(options, retryPolicy) {
         super(options);
         this.retryPolicy = retryPolicy;
     }
-    fetch(method, params, id) {
-        return (0, Utils_1.tryWithRetries)(() => super.fetch(method, params, id), this.retryPolicy, e => {
+    fetchEndpoint(method, params) {
+        return (0, Utils_1.tryWithRetries)(() => super.fetchEndpoint(method, params), this.retryPolicy, e => {
             if (!(e instanceof starknet_1.RpcError))
                 return false;
             if (e.baseError.code < 0)
                 return false; //Not defined error, e.g. Rate limit (-32097)
             return true;
         });
+    }
+}
+exports.RpcChannelWithRetries = RpcChannelWithRetries;
+class RpcProviderWithRetries extends starknet_1.RpcProvider {
+    constructor(options, retryPolicy) {
+        super(options);
+        this.channel = new RpcChannelWithRetries({ ...options, waitMode: false }, retryPolicy);
     }
 }
 exports.RpcProviderWithRetries = RpcProviderWithRetries;
--- src/starknet/provider/RpcProviderWithRetries.ts
+++ src/starknet/provider/RpcProviderWithRetries.ts
@@ -1,26 +1,41 @@
-import {ProviderInterface, RpcError, RpcProvider, RpcProviderOptions} from "starknet";
+import {
+    RpcChannel,
+    RpcError,
+    RpcProvider,
+    RpcProviderOptions
+} from "starknet";
 import {tryWithRetries} from "../../utils/Utils";
 
-
-export class RpcProviderWithRetries extends RpcProvider {
+export class RpcChannelWithRetries extends RpcChannel {
 
     readonly retryPolicy?: {
         maxRetries?: number, delay?: number, exponential?: boolean
     };
 
-    constructor(options?: RpcProviderOptions | ProviderInterface, retryPolicy?: {
+    constructor(options?: RpcProviderOptions, retryPolicy?: {
         maxRetries?: number, delay?: number, exponential?: boolean
     }) {
         super(options);
         this.retryPolicy = retryPolicy;
     }
 
-    fetch(method: string, params?: object, id?: string | number): Promise<Response> {
-        return tryWithRetries(() => super.fetch(method, params, id), this.retryPolicy, e => {
+    protected fetchEndpoint(method: any, params?: any): Promise<any> {
+        return tryWithRetries(() => super.fetchEndpoint(method, params), this.retryPolicy, e => {
             if(!(e instanceof RpcError)) return false;
             if(e.baseError.code<0) return false; //Not defined error, e.g. Rate limit (-32097)
             return true;
         });
+    }
+
+}
+
+export class RpcProviderWithRetries extends RpcProvider {
+
+    constructor(options?: RpcProviderOptions, retryPolicy?: {
+        maxRetries?: number, delay?: number, exponential?: boolean
+    }) {
+        super(options);
+        this.channel = new RpcChannelWithRetries({ ...options, waitMode: false }, retryPolicy);
     }
 
 }

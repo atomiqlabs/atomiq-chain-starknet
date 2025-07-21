d989ad25f031f2b8f3fd803a0095d400f0ed14cb
adambor <adamborteam@gmail.com>
2025-07-21 02:39:54 +0200

    Fix not check signatures when not needed

--- dist/starknet/swaps/modules/StarknetSwapInit.d.ts
+++ dist/starknet/swaps/modules/StarknetSwapInit.d.ts
@@ -43,6 +43,7 @@ export declare class StarknetSwapInit extends StarknetSwapModule {
     /**
      * Checks whether the provided signature data is valid, using preFetchedData if provided and still valid
      *
+     * @param sender
      * @param swapData
      * @param timeout
      * @param prefix

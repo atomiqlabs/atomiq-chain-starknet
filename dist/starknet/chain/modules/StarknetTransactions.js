77bda59616894344bc048fa469e2066ddd1feddb
adambor <adamborteam@gmail.com>
2025-08-17 10:43:19 +0200

    Add maximum unconfirmed txns limit

--- dist/starknet/chain/modules/StarknetTransactions.js
+++ dist/starknet/chain/modules/StarknetTransactions.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.StarknetTransactions = void 0;
 const StarknetModule_1 = require("../StarknetModule");
 const Utils_1 = require("../../../utils/Utils");
+const MAX_UNCONFIRMED_TXS = 25;
 class StarknetTransactions extends StarknetModule_1.StarknetModule {
     constructor() {
         super(...arguments);
@@ -154,7 +155,7 @@ class StarknetTransactions extends StarknetModule_1.StarknetModule {
             " waitForConfirmation: " + waitForConfirmation + " parallel: " + parallel);
         const txIds = [];
         if (parallel) {
-            const promises = [];
+            let promises = [];
             for (let i = 0; i < txs.length; i++) {
                 const signedTx = txs[i];
                 const txId = await this.sendSignedTransaction(signedTx, onBeforePublish, signer);
@@ -162,6 +163,10 @@ class StarknetTransactions extends StarknetModule_1.StarknetModule {
                     promises.push(this.confirmTransaction(signedTx, abortSignal));
                 txIds.push(txId);
                 this.logger.debug("sendAndConfirm(): transaction sent (" + (i + 1) + "/" + txs.length + "): " + signedTx.txId);
+                if (promises.length >= MAX_UNCONFIRMED_TXS) {
+                    await Promise.all(promises);
+                    promises = [];
+                }
             }
             if (promises.length > 0)
                 await Promise.all(promises);

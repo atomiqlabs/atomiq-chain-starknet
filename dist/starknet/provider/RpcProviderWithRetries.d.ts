1c00d87d206d9e0eb3c8246568fac6f0aff38669
adambor <adamborteam@gmail.com>
2025-09-10 23:27:35 +0200

    Separate v0.8 and v0.9 channels, try to do naive auto-detection

--- dist/starknet/provider/RpcProviderWithRetries.d.ts
+++ dist/starknet/provider/RpcProviderWithRetries.d.ts
@@ -1,5 +1,18 @@
-import { RpcChannel, RpcProvider, RpcProviderOptions } from "starknet";
-export declare class RpcChannelWithRetries extends RpcChannel {
+import { RPC08, RPC09, RpcProvider, RpcProviderOptions } from "starknet";
+export declare class Rpc08ChannelWithRetries extends RPC08.RpcChannel {
+    readonly retryPolicy?: {
+        maxRetries?: number;
+        delay?: number;
+        exponential?: boolean;
+    };
+    constructor(options?: RpcProviderOptions, retryPolicy?: {
+        maxRetries?: number;
+        delay?: number;
+        exponential?: boolean;
+    });
+    protected fetchEndpoint(method: any, params?: any): Promise<any>;
+}
+export declare class Rpc09ChannelWithRetries extends RPC09.RpcChannel {
     readonly retryPolicy?: {
         maxRetries?: number;
         delay?: number;
@@ -13,6 +26,13 @@ export declare class RpcChannelWithRetries extends RpcChannel {
     protected fetchEndpoint(method: any, params?: any): Promise<any>;
 }
 export declare class RpcProviderWithRetries extends RpcProvider {
+    /**
+     * Tries to do naive detection of the spec version based on the suffix of nodeUrl, better pass the `specVersion`
+     *  in the options!
+     *
+     * @param options
+     * @param retryPolicy
+     */
     constructor(options?: RpcProviderOptions, retryPolicy?: {
         maxRetries?: number;
         delay?: number;
--- dist/starknet/provider/RpcProviderWithRetries.js
+++ dist/starknet/provider/RpcProviderWithRetries.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.RpcProviderWithRetries = exports.RpcChannelWithRetries = void 0;
+exports.RpcProviderWithRetries = exports.Rpc09ChannelWithRetries = exports.Rpc08ChannelWithRetries = void 0;
 const starknet_1 = require("starknet");
 const Utils_1 = require("../../utils/Utils");
-class RpcChannelWithRetries extends starknet_1.RpcChannel {
+class Rpc08ChannelWithRetries extends starknet_1.RPC08.RpcChannel {
     constructor(options, retryPolicy) {
         super(options);
         this.retryPolicy = retryPolicy;
@@ -22,11 +22,45 @@ class RpcChannelWithRetries extends starknet_1.RpcChannel {
         });
     }
 }
-exports.RpcChannelWithRetries = RpcChannelWithRetries;
-class RpcProviderWithRetries extends starknet_1.RpcProvider {
+exports.Rpc08ChannelWithRetries = Rpc08ChannelWithRetries;
+class Rpc09ChannelWithRetries extends starknet_1.RPC09.RpcChannel {
     constructor(options, retryPolicy) {
         super(options);
-        this.channel = new RpcChannelWithRetries({ ...options, waitMode: false }, retryPolicy);
+        this.retryPolicy = retryPolicy;
+    }
+    fetchEndpoint(method, params) {
+        return (0, Utils_1.tryWithRetries)(() => super.fetchEndpoint(method, params), this.retryPolicy, e => {
+            if (!e.message.startsWith("RPC: "))
+                return false;
+            const arr = e.message.split("\n");
+            const errorCode = parseInt(arr[arr.length - 1]);
+            if (isNaN(errorCode))
+                return false;
+            if (errorCode < 0)
+                return false; //Not defined error, e.g. Rate limit (-32097)
+            return true;
+        });
+    }
+}
+exports.Rpc09ChannelWithRetries = Rpc09ChannelWithRetries;
+class RpcProviderWithRetries extends starknet_1.RpcProvider {
+    /**
+     * Tries to do naive detection of the spec version based on the suffix of nodeUrl, better pass the `specVersion`
+     *  in the options!
+     *
+     * @param options
+     * @param retryPolicy
+     */
+    constructor(options, retryPolicy) {
+        if (options.specVersion == null)
+            options.specVersion = options.nodeUrl.endsWith("v0_8") ? "0.8.1" : "0.9.0";
+        super(options);
+        if (this.channel.id === "RPC081") {
+            this.channel = new Rpc08ChannelWithRetries({ ...options, waitMode: false }, retryPolicy);
+        }
+        else if (this.channel.id === "RPC090") {
+            this.channel = new Rpc09ChannelWithRetries({ ...options, waitMode: false }, retryPolicy);
+        }
     }
 }
 exports.RpcProviderWithRetries = RpcProviderWithRetries;

a81dd37bdb8901fc4417e4d8938125a21a925ea7
adambor <adamborteam@gmail.com>
2025-09-30 10:24:56 +0200

    Release next version

--- dist/starknet/wallet/StarknetKeypairWallet.d.ts
+++
@@ -1,7 +0,0 @@
-import { Account, DeployAccountContractPayload, Provider } from "starknet";
-export declare class StarknetKeypairWallet extends Account {
-    readonly publicKey: string;
-    constructor(provider: Provider, privateKey: string);
-    getDeploymentData(): DeployAccountContractPayload;
-    static generateRandomPrivateKey(): string;
-}
